417dcf05ede236973c9bbe8ad303b7832e118a0f
Samuel de Jesús <76408737+Samu330@users.noreply.github.com>
2024-05-23 12:44:17 -0500

    Update process_edited_messages.js

--- scripts/process_edited_messages.js
+++ scripts/process_edited_messages.js
@@ -1,7 +1,7 @@
 const handle_edited_message = function () {
     const message = arguments[0];
     message.type = "chat";
-    message.body = `✏️ This message was edited to: ${message?.body || message?.caption}`;
+    message.body = `✏️ Sé ha editado este mensaje, el nuevo mensaje es:\n${message?.body || message?.caption}`;
     if (!message.protocolMessageKey) {
         return true;
     }
