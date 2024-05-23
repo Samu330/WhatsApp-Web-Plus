be04cb0163a7c493ff2c1d716ac12a567a846278
Samuel de Jesús <76408737+Samu330@users.noreply.github.com>
2024-05-23 13:17:57 -0500

    Update module_raid.js

--- scripts/module_raid.js
+++ scripts/module_raid.js
@@ -24,5 +24,5 @@ const initialize_modules = () => {
         };
     }
 
-    Alert(`Modules have been loaded successfully!\nBy Samu330 🍟`);
+    alert(`Modules have been loaded successfully!\nBy Samu330 🍟`);
 };
